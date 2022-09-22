3acd33820dcf5ff6ca2077c6c16fd0a9d2be4e8b
php1301 <19520854@gm.uit.edu.vn>
2022-09-22 18:49:14 +0000

    Test DAG config

--- dags/simple_pipe.py
+++ dags/simple_pipe.py
@@ -11,8 +11,8 @@ default_args = {
     "email": ["support@airflow.com"],
     "email_on_failure": False,
     "email_on_retry": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5)
+    # "retries": 1,
+    # "retry_delay": timedelta(minutes=5)
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
